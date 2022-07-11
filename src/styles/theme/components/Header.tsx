4def174cd02db8978d9c358c275de87dc4545ffe
Raique Ramos <96999326+RaiqueDev@users.noreply.github.com>
2022-07-11 10:44:43 -0300

    second commit

---
+++ src/styles/theme/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "../../images/Logo";
+
+export function Header() {
+  return (
+    <>
+      <header className="w-full flex items-center justify-center py-5 bg-gray-700 border-b border-b-gray-600">
+        <Logo />
+      </header>
+    </>
+  )
+}
